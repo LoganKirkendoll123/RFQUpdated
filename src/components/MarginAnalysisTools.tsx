e9a82809d28e70fea7af98ec58e102f66fe0c361
LoganKirkendoll123 <logankirkendoll25@gmail.com>
2025-07-06 20:55:41 -0500

    Fix margin analysis component props and state management

--- src/components/MarginAnalysisTools.tsx
+++ src/components/MarginAnalysisTools.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { CarrierScorecard } from './CarrierScorecard';
 import { AIForecasting } from './AIForecasting';
 import { NegotiationAnalysis } from './NegotiationAnalysis';
